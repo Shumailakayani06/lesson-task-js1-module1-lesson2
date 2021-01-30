7b786fa6a7323e8504ac89a886b59fa2d8a9f2b5
Shumaila Kayani <shumailakayani059@gmail.com>
2021-01-30 23:01:59 +0100

    hello

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 //Question 1
 
-document.querySelector(h3) 
-//h3.innerHTML("Welcome")
+document.querySelector("h3").innerHTML = "hello";
+
+//Question 2
+
+let heading4 = document.querySelectorAll("h4")
+let names = ["Mehtab","Shumaila","Ali"]
+for(let i = 0; i < heading4.length; i++){
+    console.log(heading4[i].style.marginBottom = "-20")
+    console.log(heading4[1].style.color = "red")
+}
+
+
+//Question 3
+
+//Question 4
+
+//Question 5
+
+
+const thirdParagraph = document.querySelector("p.three");
+thirdParagraph.innerHTML = "<span>" + thirdParagraph.innerHTML + "</span>";
+
+
+
+
+
+
+
